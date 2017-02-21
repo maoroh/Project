eb0787e2a3c816cfb8fe2121e1a3ec52d8c4727e
maor <maor@gmail.com>
2017-02-21 20:54:40 +0200

    maor

--- encryptor/src/main/java/com/maor/encryptor/App.java
+++ encryptor/src/main/java/com/maor/encryptor/App.java
@@ -1,5 +1,7 @@
 package com.maor.encryptor;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Please enter the path : " );
+        Scanner in = new Scanner(System.in);
+        String path = in.nextLine();
+        CaesarCipher.encrypt(120, path);
+        CaesarCipher.decrypt(120, "out.txt");
+        in.close();
     }
 }
---
+++ encryptor/src/main/java/com/maor/encryptor/CaesarCipher.java
@@ -0,0 +1,84 @@
+package com.maor.encryptor;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CaesarCipher {
+	
+	public static void encrypt(int key, String path)
+	{
+		File file = new File(path);
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		try {
+			fis = new FileInputStream(file);
+			fos = new FileOutputStream("out.txt");
+			System.out.println("Total file size to read (in bytes) : "
+					+ fis.available());
+
+			int content;
+			while ((content = fis.read()) != -1) {
+				System.out.println(content);
+				if(content + key > Byte.MAX_VALUE)
+					content = (content + key - Byte.MAX_VALUE - 1) + Byte.MIN_VALUE; 
+				else content += key;
+				System.out.println(content);
+				fos.write(content);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		
+		} finally {
+			try {
+					fis.close();
+					fos.close();
+				}
+				catch (NullPointerException ex) {
+				ex.printStackTrace();
+			}
+			catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	
+	public static void decrypt(int key, String path)
+	{
+		File file = new File(path);
+		FileInputStream fis = null;
+		FileOutputStream fos = null;
+		try {
+			fis = new FileInputStream(file);
+			fos = new FileOutputStream("out2.txt");
+			System.out.println("Total file size to read (in bytes) : "
+					+ fis.available());
+
+			int content;
+			while ((content = fis.read()) != -1) {
+				// convert to char and display it
+				if(content - key < Byte.MIN_VALUE)
+					content = (content - key - Byte.MIN_VALUE + 1) + Byte.MAX_VALUE; 
+				else content -= key;
+				fos.write(content);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+					fis.close();
+					fos.close();
+			} catch (NullPointerException ex) {
+				ex.printStackTrace();
+			}
+			catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+	}
+}
